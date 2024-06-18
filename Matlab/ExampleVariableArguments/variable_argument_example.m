cb47abd5bdd1603a27ec0b4d1de350fde7d4ec66
Greg Conradi Smith <greg.conradi.smith@gmail.com>
2024-06-18 19:17:42 -0400

    Update variable_argument_example.m

--- Matlab/ExampleVariableArguments/variable_argument_example.m
+++ Matlab/ExampleVariableArguments/variable_argument_example.m
@@ -1,14 +1,22 @@
 function variable_argument_example(varargin)
+% VARIABLE_ARGUMENT_EXAMPLE - A function demonstrating the use of variable input arguments
 %
-% EXAMPLE: variable_argument_example()
-%          variable_argument_example(pi,sqrt(2),10)
-% 
+% EXAMPLES:
+%   variable_argument_example()
+%   variable_argument_example(pi, sqrt(2), 10)
+%
+% This function takes a variable number of input arguments and displays each one.
 
-disp(['Number of arguments passed = ' num2str(nargin) '.'])
-if nargin>0
-    for i=1:nargin
-    disp(['   Argument ' num2str(i) ' = ' num2str(varargin{i}) '.'])
+    % Display the number of arguments passed to the function
+    disp(['Number of arguments passed = ' num2str(nargin) '.'])
+
+    % Check if there are any arguments passed
+    if nargin > 0
+        % Loop through each argument passed to the function
+        for i = 1:nargin
+            % Display the argument number and its value
+            disp(['   Argument ' num2str(i) ' = ' num2str(varargin{i}) '.'])
+        end
     end
-end
 
-return
+end
